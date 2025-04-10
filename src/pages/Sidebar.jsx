78112d2ce35ebc7ded423f49c0a981ae2d9c0050
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-04-10 17:27:50 +0800

    left

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
 
   return (
       
-    <div className=" w-64  h-screen bg-gray-800 text-white p-6 fixed">
+    <div className=" w-64  h-screen bg-gray-800 text-white p-6 fixed left-0">
       <h2 className="text-xl font-bold mb-6">Asset Management System</h2>
       <ul className="space-y-3">
         <li

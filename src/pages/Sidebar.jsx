7fe7f97966fbcad78dbc9dbb68c07fe575d6416c
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-04-10 16:30:29 +0800

    sidebars

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-68 h-screen bg-gray-800 text-white p-6 fixed top-0 left-0">
+    <div className="w-64 h-screen bg-gray-800 text-white p-6 fixed top-16 left-0">
       <h2 className="text-xl font-bold mb-6">Asset Management System</h2>
       <ul className="space-y-3">
         <li

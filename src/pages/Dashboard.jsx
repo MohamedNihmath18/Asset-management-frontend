f2ca2ec1caf326adcb347490e72d304b14cff9bd
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-04-10 17:39:52 +0800

    ml-64

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -75,7 +75,7 @@ const Dashboard = () => {
       
 
       {/* Main Dashboard */}
-      <div className="ml-64 flex-1 p-6 mt-16">
+      <div className="ml-60 flex-1 p-6 mt-16">
         <h1 className="text-3xl font-bold text-blue-700">Dashboard</h1>
 
         {/* Stats Section */}

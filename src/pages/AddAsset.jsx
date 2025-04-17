a3385cdd4f23157efca0a02f09171b11eec349d2
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-04-17 10:47:34 +0800

    commit

--- src/pages/AddAsset.jsx
+++ src/pages/AddAsset.jsx
@@ -81,7 +81,7 @@ const AddAsset = () => {
       {/* Header */}
       <h1 className="text-3xl font-bold text-blue-700 text-center">Add New Asset</h1>
 
-      <form className="mt-6 bg-white shadow-md p-6 rounded-lg" onSubmit={handleSubmit}>
+      <form className="mt-6 bg-white shadow-md p-6 rounded-lg" onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {/* Equipment Details */}
           {[
